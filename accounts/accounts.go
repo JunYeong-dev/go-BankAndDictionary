028bd2be55fbd1b18df98f05c26c5fd692259d6a
Jun-cloud <kjy8827@gmail.com>
2020-06-05 23:08:18 +0900

    Create Deposit Method

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -12,3 +12,18 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Deposit - method, Account의 balance 값을 변경
+// (a Account) : receiver - a는 이름이고 type은 Account
+// 이름을 짓는 규칙은 struct의 첫 글자를 따서 '소문자'로 지어야 함
+// 기본적으로 Go의 경우 (a Account)의 형태로 recevier로 하는 경우 복사본을 만들게 됨
+// main.go 쪽의 account에 접근 하는 것이 아닌 복사본의 amount의 값을 변경하게 되는 것
+// 그렇기 때문에 직접 값을 변경 하기 위해선 pointer로 접근하는 것이 필요함
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance - Account의 balance 값을 return
+func (a Account) Balance() int {
+	return a.balance
+}
--- main.go
+++ main.go
@@ -9,4 +9,6 @@ import (
 func main() {
 	account := accounts.NewAccount("judy")
 	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
 }
